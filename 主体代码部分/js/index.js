e3fc91d254ccafb0d42b979026b28e3bcc353c3c
Your Name <1479085300qq.com>
2018-07-25 14:17:34 +0800

    commit with nav bar2

--- 主体代码部分/js/index.js
+++ 主体代码部分/js/index.js
@@ -1,128 +1,236 @@
 //
-// $(function () {
-//     FastClick.attach(document.body);
+$(function () {
+    FastClick.attach(document.body);
+
+    /*用于获取首页事件的变量*/
+    var page0 = $("#page0");
+    var page1 = $("#page1");
+    var page2 = $("#page2");
+    var page3 = $("#page3");
+    var page4 = $("#page4");
+    var page5 = $("#page5");
+    var page6 = $("#page6");
+    var page7 = $("#page7");
+    var page8 = $("#page8");
+    var page9 = $("#page9");
+    var page10 = $("#page10");
+    var page11 = $("#page11");
+    var page12 = $("#page12");
+    var page13 = $("#page13");
+    var page14 = $("#page14");
+    var page15 = $("#page15");
+    var page16 = $("#page16");
+    var page17 = $("#page17");
+    var page18 = $("#page18");
+    var page19 = $("#page19");
+    var page20 = $("#page20");
+    var page21 = $("#page21");
+    var page22 = $("#page22");
+    var page23 = $("#page23");
+    var page24 = $("#page24");
+    var page25 = $("#page25");
+    var page26 = $("#page26");
+    var page27 = $("#page27");
+    var page28 = $("#page28");
+    var page29 = $("#page29");
+    var page30 = $("#page30");
+    var page31 = $("#page31");
+    var page32 = $("#page32");
+    var page33 = $("#page33");
+    var page34 = $("#page34");
+    var page35 = $("#page35");
+    var page36 = $("#page36");
+    var page37 = $("#page37");
+    var page38 = $("#page38");
+    var page39 = $("#page39");
+    var page40 = $("#page40");
+    var page41 = $("#page41");
+    var page42 = $("#page42");
+    var page43 = $("#page43");
+    var page44 = $("#page44");
+    var page45 = $("#page45");
+    var page46 = $("#page46");
+    var page47= $("#page47");
+    var page48 = $("#page48");
+    var page49 = $("#page49");
+    var page50 = $("#page50");
+    var page51 = $("#page51");
+    var page52 = $("#page52");
+    var page53 = $("#page53");
+    var page54 = $("#page53");
+    var page55 = $("#page55");
+    var page56 = $("#page56");
+    var page57 = $("#page57");
+    var page58 = $("#page58");
+    var page59 = $("#page59");
+    var page60 = $("#page60");
+    var page61 = $("#page61");
+    var page62 = $("#page62");
+    var page63 = $("#page63");
+    var page64 = $("#page64");
+    var page65 = $("#page65");
+    var page66 = $("#page66");
+
+    var nav1 = $("#nav1");
+    var nav2 = $("#nav2");
+    var nav3 = $("#nav3");
+    var nav4 = $("#nav4");
+    var nav5 = $("#nav5");
+    var nav6 = $("#nav6");
+    var nav7 = $("#nav7");
+    var nav8 = $("#nav8");
+    var nav9 = $("#nav9");
+    var nav10 = $("#nav10");
+    var nav11 = $("#nav11");
+    var nav12 = $("#nav12");
+    var nav13 = $("#nav13");
+    var nav14 = $("#nav14");
+    var nav15 = $("#nav15");
+    var nav16 = $("#nav16");
+    var nav17 = $("#nav17");
+    var nav18 = $("#nav18");
+    var nav19 = $("#nav19");
+    var nav20 = $("#nav20");
+    var nav21 = $("#nav21");
+    var nav22 = $("#nav22");
+    var nav23 = $("#nav23");
+    var nav24 = $("#nav24");
+    var nav25 = $("#nav25");
+    var nav26 = $("#nav26");
+    var nav27 = $("#nav27");
+    var nav28 = $("#nav28");
+    var nav29 = $("#nav29");
+    var nav30 = $("#nav30");
+    var nav31 = $("#nav31");
+    var nav32 = $("#nav32");
+    var nav33 = $("#nav33");
+    var nav34 = $("#nav34");
+    var nav35 = $("#nav35");
+    var nav36 = $("#nav36");
+    var nav37 = $("#nav37");
+    var nav38 = $("#nav38");
+    var nav39 = $("#nav39");
+    var nav40 = $("#nav40");
+    var nav41 = $("#nav41");
+    var nav42 = $("#nav42");
+    var nav43 = $("#nav43");
+    var nav44 = $("#nav44");
+    var nav45 = $("#nav45");
+    var nav46 = $("#nav46");
+    var nav47 = $("#nav47");
+    var nav48= $("#nav48");
+    var nav49 = $("#nav49");
+    var nav50 = $("#nav50");
+    var nav51 = $("#nav51");
+    var nav52 = $("#nav52");
+    var nav53 = $("#nav53");
+    var nav54 = $("#nav54");
+    var nav55 = $("#nav55");
+    var nav56 = $("#nav56");
+    var nav57 = $("#nav57");
+    var nav58 = $("#nav58");
+    var nav59 = $("#nav59");
+    var nav60 = $("#nav60");
+    var nav61 = $("#nav61");
+    var nav62 = $("#nav62");
+    var nav63 = $("#nav63");
+    var nav64 = $("#nav64");
+    var nav65 = $("#nav65");
+    var nav66 = $("#nav66");
+    var nav67 = $("#nav67");
+
+
+
+    navJump(page0,nav2,page2);/*学校简介*/
+    navJump(page0,nav3,page3);/*学校章程*/
+    navJump(page0,nav4,page4);/*南邮校史*/
+    navJump(page0,nav6,page16);/*电子地图*/
+   /* navJump(page0,nav7,page2);暂无*//*全景地图*/
+    // navJump(page0,);
+    // navJump(page0,);
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+    // navJump();
+});
 //
-//     /*用于获取首页事件的变量*/
-//     var jump;
-//     /*首页七个链接的变量*/
-//     var jump1,jump2,jump3,jump4,jump5,jump6,jump7;
-//     jump = $(".freshman_zone");
-//     jump1 = $(".slide1");/*首页 - 南邮概况*/
-//     jump2 = $(".slide2");/*首页 - 新生导航*/
-//     jump3 = $(".slide3");/*首页 - 生活攻略*/
-//     jump4 = $(".slide4");/*首页 - 院系概览*/
-//     jump5 = $(".slide5");/*首页 - 军训攻略*/
-//     jump6 = $(".slide6");/*首页 - 社团初探*/
-//     jump7 = $(".slide7");/*首页 - 技能学习*/
-//     jump8 = $(".slide8");/*首页 - 考研求职*/
-//     /*用于获取南邮概况事件的变量*/
-//     var overview = $(".overview_njupt");
-//     PageJump(jump,jump1,overview);
-//     /*用于获取南邮概况界面6个模块事件变量*/
-//     var overview1,overview2,overview3,overview4,overview5,overview6;
-//     overview1 = $(".overview1");/*南邮概况 - 学校简介*/
-//     overview2 = $(".overview2");/*南邮概况 - 学校章程*/
-//     overview3 = $(".overview3");/*南邮概况 - 南邮校史*/
-//     overview4 = $(".overview4");/*南邮概况 - 现任领导*/
-//     overview5 = $(".overview5");/*南邮概况 - 电子地图*/
-//     overview6 = $(".overview6");/*南邮概况 - 360°南邮全景地图*/
-//     /*获取南邮概况内部内容*/
-//     var college1,college2,college3,college4,college5,college6;
-//     college1 = $(".college_intro");
-//     college2 = $(".college_rule");
-//     college3 = $(".college_history");
-//     college4 = $(".now_leader");
-//     college5 = $(".electronicMap");
-//     // college6 = $(".college_intro");
-//     PageJump(overview,overview1,college1);
-//     PageJump(overview,overview2,college2);
-//     PageJump(overview,overview3,college3);
-//     PageJump(overview,overview4,college4);
-//     PageJump(overview,overview5,college5);
-//     /*领导介绍列表*/
-//     var leader1,leader2,leader3,leader4,leader5,leader6,leader7,leader8,leader9,leader10,
-//     leader1 = $(".leader1");
-//     leader2 = $(".leader2");
-//     leader3 = $(".leader3");
-//     leader4 = $(".leader4");
-//     leader5 = $(".leader5");
-//     leader6 = $(".leader6");
-//     leader7 = $(".leader7");
-//     leader8 = $(".leader8");
-//     leader9 = $(".leader9");
-//     leader10 = $(".leader10");
-//     /*领导介绍内容*/
-//     var liuchen = $(".liuchen");/*刘陈*/
-//     var yangzhen = $(".yangzhen");/*杨震*/
-//     var wck = $(".wangchengkuan");/*王成宽*/
-//     var wgp = $(".wangguoping");/*王国平*/
-//     var wzr = $(".wangzongrong");/*王宗荣*/
-//     var jgp = $(".jiangguoping");/*蒋国平*/
-//     var zzh = $(".zhangzhihua");/*张志华*/
-//     var znp = $(".zhounanping");/*周南平*/
-//     var lzq = $(".liziquan");/*李子全*/
-//     var wlh = $(".wanglianhui");/*汪联辉*/
-//     PageJump(college4,leader1,liuchen);
-//     PageJump(college4,leader2,yangzhen);
-//     PageJump(college4,leader3,wck);
-//     PageJump(college4,leader4,wgp);
-//     PageJump(college4,leader5,wzr);
-//     PageJump(college4,leader6,jgp);
-//     PageJump(college4,leader7,zzh);
-//     PageJump(college4,leader8,znp);
-//     PageJump(college4,leader9,lzq);
-//     PageJump(college4,leader10,wlh);
-//     /*南邮概况 - 电子地图内容*/
-//     var xianlin_map = $(".xianlin_map");
-//     var san_map = $(".san_map");
-//     var suo_map = $(".suo_map");
-//     /*电子地图列表*/
-//     var xianlin = $(".bgColor5");
-//     var sanPL = $(".bgColor6");
-//     var suoJC = $(".bgColor7");
-//     PageJump(college5,xianlin,xianlin_map);
-//     PageJump(college5,sanPL,san_map);
-//     PageJump(college5,suoJC,suo_map);
-//
-//     /*新生导航跳转*/
-//     /*jump2点击跳转*/
-//     /*新生导航界面*/
-//     var freshmen_guide = $(".Guide_system");
-//     PageJump(jump,jump2,freshmen_guide);
-//     var guide1 = $(".guide1");/*新生导航 - 报到流程*/
-//     var guide2 = $(".guide2");/*新生导航 - 新生报到推荐单*/
-//     var guide3 = $(".guide3");/*新生导航 - 学长学姐建议*/
-//     var guide4 = $(".guide4");/*新生导航 - 入学小贴士*/
-//
-//     var procedure = $('.register_procedure');/*内容 - 报到流程*/
-//     var list = $(".register_list");/*内容 - 新生报到推荐单*/
-//     var suggest = $(".suggestion");/*内容 - 学长学姐建议*/
-//     var tips = $(".register_tips");/*内容 - 入学小贴士*/
-//     PageJump(freshmen_guide,guide1,procedure);
-//     PageJump(freshmen_guide,guide2,list);
-//     PageJump(freshmen_guide,guide3,suggest);
-//     PageJump(freshmen_guide,guide4,tips);
-// });
-//
-// /*PageJump函数：
-// * 第一个参数：需要隐藏的界面；
-// * 第二个参数：点击的界面；
-// * 第三个参数：需要显示的界面；
-// * */
-// var PageJump = function (e1,e,e2) {
-//     e.unbind("touchend").on("touchend",function () {
-//         if (e.cancelable) {
-//             // 判断默认行为是否已经被禁用
-//             if (!e.defaultPrevented) {
-//                 e.preventDefault();
-//             }
-//         };
-//         console.log("123");
-//         e1.addClass("hidden");
-//         e2.removeClass("hidden");
-//     });
-// }
-//
+/*navJump函数：
+*
+*  第一个参数：需要隐藏的界面；
+*  第二个参数：点击的导航上的链接；
+*  第三个参数：要显示的界面；
+* */
+var navJump = function (e1,e,e2) {
+    e.click(function () {
+        console.log("bug");
+        if (e.cancelable) {
+            // 判断默认行为是否已经被禁用
+            if (!e.defaultPrevented) {
+                e.preventDefault();
+            }
+        }
+        console.log("123");
+        e1.addClass("hidden");
+        e2.removeClass("hidden");
+        // var drawer = $(".drawer-list");
+        // drawer.addClass("hidden");
+    });
+
+};
+
 //
 //
 //
